c96a6d1885acf81132d6fbaffce6349dc1a5b1e3
Alex Wendland <me@alexwendland.com>
2020-12-21 15:29:44 -0800

    feat(benchmark): add initial implementation of the benchmark runner

--- README.md
+++ README.md
@@ -4,12 +4,27 @@ Comparison of CSV parsing and writing performance across multiple languages.
 
 ## Benchmark Structure
 
-Each benchmark will self-report performance statistics. This is so extraneous runtime costs, such as initial file creation, runtime engine startup, module imports, and other such costs that will be amortized away in massive CSV processing operations do not skew the results. They will write (append) their performance results as newline-delimited JSON to `$BENCHMARK_DIR/performance`. The JSON will match the following schema:
+Each benchmark will self-report performance statistics. This is so extraneous runtime costs, such as initial file creation, runtime engine startup, module imports, and other such costs that will be amortized away in massive CSV processing operations do not skew the results.
+
+### Benchmark Results
+
+Each benchmark will write (append) their performance results as newline-delimited JSON to `$BENCHMARK_DIR/performance`. The JSON will match the following schema:
 
 ```typescript
 {
   "finished_at": string, // ISO 8601 date time
   "duration": number, // milliseconds
-  "in_csv": string // path of input CSV file
+  "env_args": {} // all environment variables beginning with BENCH_
+}
+```
+
+### Benchmark Manifest
+
+Each benchmark will provide a JSON manifest at `$BENCHMARK_DIR/benchmark.json`. This manifest will contain:
+
+```typescript
+{
+  "check:environment": string[], // command for loosely checking if the host environment has been correctly setup to run the benchmark
+  "run:csv_to_tsv": string[] // command to run a benchmark converting a CSV to a TSV; expects BENCH_IN_CSV and BENCH_IN_TSV to be present in env vars
 }
 ```
---
+++ benchmark.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+import argparse
+import subprocess
+import json
+import logging
+import sys
+import shutil
+
+benchmark_manifests = list(Path(__file__).parent.glob("*/benchmark.json"))
+benchmark_names = {p.parent.stem: p for p in benchmark_manifests}
+
+parser = argparse.ArgumentParser(description="Execute CSV processing benchmarks")
+parser.add_argument(
+    "benchmarks",
+    nargs="+",
+    choices=["all"] + list(benchmark_names.keys()),
+    help="which benchmarks to execute",
+)
+parser.add_argument(
+    "--log-level",
+    default=logging.INFO,
+    type=lambda l: getattr(logging, l),
+    help="logging level",
+)
+parser.add_argument(
+    "--mode",
+    default="csv_to_csv",
+    choices=["csv_to_tsv"],
+    help="which CSV process to benchmark",
+)
+parser.add_argument(
+    "--input-csv",
+    default=Path(__file__).parent / "data-50k-lines.csv",
+    type=lambda p: Path(p).resolve(),
+    help="the well-formed CSV file to use as benchmark input",
+)
+args = parser.parse_args()
+
+logging.basicConfig(
+    format=f'{"" if sys.stderr.isatty() else "%(levelname)s - "}%(message)s',
+    level=args.log_level,
+)
+
+logging.info(
+    "Found %d lines in input CSV file '%s'",
+    sum(1 for line in open(args.input_csv)),
+    args.input_csv,
+)
+
+if "all" in args.benchmarks:
+    args.benchmarks = list(benchmark_names.keys())
+
+results = {}
+
+for benchmark in args.benchmarks:
+    benchmark_manifest = benchmark_names[benchmark]
+    benchmark_dir = benchmark_manifest.parent
+    benchmark_performance = benchmark_dir / "performance"
+    logging.info("BENCHMARK: %s", benchmark)
+    try:
+        benchmark_suite = json.loads(benchmark_manifest.read_text("utf8"))
+
+        CMD_ENV_VARS = {
+            "BENCH_IN_CSV": args.input_csv.resolve(),
+            "BENCH_OUT_TSV": (benchmark_dir / "out.tsv").resolve(),
+        }
+
+        cmd_check_env = benchmark_suite["check:environment"]
+        logging.info('%s: checking environment with "%s"', benchmark, cmd_check_env)
+        subprocess.run(
+            [shutil.which(cmd_check_env[0])] + cmd_check_env[1:],
+            cwd=benchmark_dir,
+            env=CMD_ENV_VARS,
+            stderr=subprocess.STDOUT,
+            stdout=None
+            if logging.getLogger().getEffectiveLevel() <= logging.INFO
+            else subprocess.DEVNULL,
+        )
+
+        logging.debug("%s: reviewing previous benchmark results", benchmark)
+        prev_results = benchmark_performance.read_text("utf8").strip().split("\n")
+        if len(prev_results) > 0:
+            prev_result = json.loads(prev_results[-1])
+            logging.debug(
+                "%s: previous benchmark was run at %s", prev_result["recorded_at"]
+            )
+
+        cmd_run_benchmark = benchmark_suite["run:csv_to_tsv"]
+        logging.info(
+            '%s: benchmarking convert CSV to TSV with "%s"',
+            benchmark,
+            cmd_run_benchmark,
+        )
+        subprocess.run(
+            [shutil.which(cmd_run_benchmark[0])] + cmd_run_benchmark[1:],
+            cwd=benchmark_dir,
+            env=CMD_ENV_VARS,
+            stderr=subprocess.STDOUT,
+            stdout=None
+            if logging.getLogger().getEffectiveLevel() <= logging.DEBUG
+            else subprocess.DEVNULL,
+        )
+
+        logging.debug("%s: loading latest benchmark results", benchmark)
+        latest_results = benchmark_performance.read_text("utf8").strip().split("\n")
+        if len(latest_results) == 0:
+            logging.error(
+                "%s: no benchmark results found at %s", benchmark, benchmark_performance
+            )
+            continue
+        latest_result = json.loads(latest_results[-1])
+        if (
+            prev_result is not None
+            and latest_result["recorded_at"] == prev_result["recorded_at"]
+        ):
+            logging.error(
+                '%s: benchmark appears to have failed (the latest timestamp in "%s" did not change)',
+                benchmark,
+                benchmark_performance,
+            )
+            continue
+        logging.info("%s: runtime was %d ms", benchmark, latest_result["duration"])
+        results[benchmark] = latest_result["duration"]
+    except Exception as e:
+        logging.exception(e)
+        results[benchmark] = "FAILED"
+
+for benchmark, result in results.items():
+    print("%s\t%s" % (benchmark, result))
