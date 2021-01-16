08379384f821e48cf5903bd45e2f95026caa145b
Alex Wendland <me@alexwendland.com>
2021-01-15 18:24:53 -0800

    feat(node-fast-csv): add initial implementation for node.js w/ fast-csv@4.3.6

---
+++ nodejs-fast-csv/benchmark.mjs
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+import { performance } from "perf_hooks";
+import * as csv from "fast-csv";
+
+const inputCsvStr = process.env["BENCH_IN_CSV"] ?? "-";
+const outputTsvStr = process.env["BENCH_OUT_TSV"] ?? "-";
+
+console.warn(`reading from '${inputCsvStr}'`);
+console.warn(`writing to '${outputTsvStr}'`);
+
+let inputCsv =
+  inputCsvStr == "-" ? process.stdin : fs.createReadStream(inputCsvStr);
+let outputTsv =
+  outputTsvStr == "-" ? process.stdout : fs.createWriteStream(outputTsvStr);
+
+const start = performance.now();
+const stream = inputCsv
+  .pipe(csv.parse({ headers: false }))
+  .pipe(csv.format({ delimiter: "\t" }))
+  .pipe(outputTsv)
+  .on("error", (error) => console.error(error))
+  .on("finish", () => {
+    const elapsed = performance.now() - start;
+    console.warn(`completed in ${elapsed} ms`);
+    fs.appendFileSync(
+      "performance",
+      JSON.stringify({
+        recorded_at: new Date().toISOString(),
+        duration: elapsed,
+        env_args: Object.fromEntries(
+          Object.entries(process.env).filter(
+            ([k, v]) => k.indexOf("BENCH_") == 0
+          )
+        ),
+      }) + "\n"
+    );
+  });
